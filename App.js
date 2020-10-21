6a56fc23ffc21f66cdb1fb7449d23ed3651fbd43
sokvebolkol <sokvebol.kol@gmail.com>
2020-10-21 13:36:27 +0700

    react navigation beginner learning

--- App.js
+++ App.js
@@ -1,6 +1,58 @@
-import React from "react";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
+// In App.js in a new project
 
-export default function App() {
-  return <WelcomeScreen />;
+import * as React from "react";
+import { View, Text, Button } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+
+const HomeScreen = ({ navigation }) => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Home Screen</Text>
+      <Button
+        title="Go to detail Screen"
+        onPress={() => navigation.navigate("Details")}
+      ></Button>
+    </View>
+  );
+};
+
+const DetainlScreen = () => {
+  return (
+    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
+      <Text>Detail Screen</Text>
+    </View>
+  );
+};
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator
+        screenOptions={{
+          headerStyle: {
+            backgroundColor: "#009387",
+          },
+          headerTintColor: "#fff",
+
+          headerTitleStyle: {
+            fontWeight: "bold",
+          },
+        }}
+      >
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{
+            title: "Overview",
+          }}
+        />
+        <Stack.Screen name="Details" component={DetainlScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
 }
+
+export default App;
